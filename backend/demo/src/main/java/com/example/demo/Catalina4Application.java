575d324f370b878c08c7e5432d569bfddd6ec0c0
Andy Agyei-Dwumah <andydwumah@gmail.com>
2026-02-23 15:36:28 +0000

    add spring boot for backend

---
+++ backend/demo/src/main/java/com/example/demo/Catalina4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Catalina4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Catalina4Application.class, args);
+	}
+
+}
