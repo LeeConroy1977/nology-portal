134592e19f0ac7f0d7cd54b383e0a7719ffe4278
Lee Conroy <leejconroy1977@gmail.com>
2026-02-23 12:54:30 +0000

    tailwind added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // scan all source files
+  ],
   theme: {
     extend: {},
+    screens: {
+      xs: "480px", // new extra small
+      sm: "640px", // default
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px", // custom extra large
+    },
   },
   plugins: [],
-}
-
+};
