cf8d3f0c01ab5fde8682a6fd8098d607d45cdacd
Andy Agyei-Dwumah <andydwumah@gmail.com>
2026-02-25 21:05:19 +0000

    add public access modifier to main class and JDK 21 to pom fileto ensure Railway compatibility

--- backend/demo/src/main/java/com/example/demo/Catalina4Application.java
+++ backend/demo/src/main/java/com/example/demo/Catalina4Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Catalina4Application {
 
-	 static void main(String[] args) {
+	 public static void main(String[] args) {
 		SpringApplication.run(Catalina4Application.class, args);
 	}
 
