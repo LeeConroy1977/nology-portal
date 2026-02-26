3785f8cf8bc05ed198b38b54f18db9c9ed9337fe
Andy Agyei-Dwumah <andydwumah@gmail.com>
2026-02-26 12:15:49 +0000

    add CORS configuration

--- backend/demo/src/main/java/com/example/demo/Catalina4Application.java
+++ backend/demo/src/main/java/com/example/demo/Catalina4Application.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Catalina4Application {
@@ -10,4 +14,18 @@ public class Catalina4Application {
 		SpringApplication.run(Catalina4Application.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(
+								"http://localhost:*"
+								)
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
