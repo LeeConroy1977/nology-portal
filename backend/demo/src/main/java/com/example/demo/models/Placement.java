d05e0f7f542851fd36f89547f6a4dd2715c93e95
JJ-blossom <73256107@protonmail.com>
2026-02-26 16:17:06 +0000

    Update json recursion path

--- backend/demo/src/main/java/com/example/demo/models/Placement.java
+++ backend/demo/src/main/java/com/example/demo/models/Placement.java
@@ -1,5 +1,6 @@
 package com.example.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 
 import java.util.List;
@@ -15,6 +16,7 @@ public class Placement {
     private User user;
 
     @OneToMany(mappedBy = "placement", cascade = CascadeType.REMOVE)
+    @JsonManagedReference
     private List<Consultant> consultants;
 
     public Long getId() {
